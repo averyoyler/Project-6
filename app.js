0890fe18c2298620891e3052945af75cb04b1853
Avery Oyler <averrryyyyyy@gmail.com>
2019-06-20 17:51:03 -0600

    add, edit, delete user functionality

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 const usersFile = require('./users.js');
 const User = usersFile.User;
 const users = usersFile.users;
-
 const fs = require('fs');
 
 let app = express();
+
+mongoose.connect('mongodb://localhost/project6', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  age: Number 
+});
+
+const user = mongoose.model('usercollection', userSchema);
+
+
+// const newUser = new user();
+//    newUser.name = 'avery';
+//    newUser.role = 'student';
+//    newUser.save((err, data) => {
+//        if (err) {
+//            return console.error(err);
+//        }
+//        console.log(`new user save: ${data}`);
+//    });
+
+
+
+
 
 let current = null;
 
@@ -19,6 +46,96 @@ app.set('view engine', 'pug');
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+// GET HOME PAGE
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+
+// GET FORM
+app.get('/form', (req, res) => {
+  res.render('pages/form');
+});
+
+// GET USERS
+app.get('/users', (req, res) => {
+  user.find({}, function(err, result) {
+    if(err) throw err;
+    res.render('pages/users', {
+      users: result
+    });
+  });
+});
+
+// GET EDIT USER
+
+app.get('/edituser/:user', (req, res) => {
+  current = req.params.user;
+  console.log(`You clicked on : ${req.params.user}`);
+  user.find({}, function(err, result) {
+    if(err) throw err;
+    res.render('pages/edit', {
+      name: req.params.user,
+      users: result,
+    });
+  });
+  
+  // user.updateOne({'name': 'Aves'}, {$set: {'name': 'aveeeeees'}})
+});
+
+
+
+
+app.post('/create', (req, res) => {
+  let newUser = new user();
+  newUser.username = req.body.username;
+  newUser.name = req.body.name;
+  newUser.email = req.body.email;
+  newUser.age = req.body.age;
+  newUser.save((err, data) => {
+    if (err) {
+        return console.error(err);
+    }
+    // console.log(`Saved User: ${data}`);
+    console.log(`Created User: ${req.body.name}`);
+    res.redirect('/users')
+  });
+});
+
+app.post('/edituser', (req, res) => {
+  let value = {'name': current};
+  let editedUser = {$set: {'username': req.body.username, 'name': req.body.name, 'email': req.body.email, 'age': req.body.age}};
+  
+  user.find({'name': req.body.name}, function(err, result) {
+    if(err) throw err;
+    user.updateOne(value, editedUser, function(err, result) {
+      if(err) throw err;
+      console.log(`Updated User: ${current}`);
+      res.redirect('/users');
+    });
+  });
+});
+
+app.get('/deleteuser/:user', (req, res) => {
+  console.log(req.params.user);
+  user.remove({'name': req.params.user}, function(err, result) {
+    if(err) throw err;
+    console.log(`Deleted User: ${req.params.user}`);
+    res.redirect('/users');
+  });
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+
+
+// OLD CODE __________________________________________
+
+/*
 app.get('/', (req, res) => {
   res.render('pages/index');
 });
@@ -86,6 +203,7 @@ app.post('/edituser', (req, res) => {
   });
 });
 
+
 app.post('/create', (req, res) => {
   let newUser = new User(req.body.username, req.body.name, req.body.email, Number(req.body.age));
   fs.readFile('./public/users.json', (err, data) => {
@@ -105,15 +223,11 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-// console.log(users.collection);
-// let jsonData = JSON.parse({users});
-// console.log(jsonData);
-
-
-
 function writeToFile() {
   fs.writeFile('./public/users.json', JSON.stringify(users.collection), (err) => {
     if(err) throw err;
     console.log('Users file updated');
   });
 }
+
+*/
